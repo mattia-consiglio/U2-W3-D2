8992912c2b17a6792841af1f90d993412e1ee8f8
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2024-01-16 19:19:36 +0100

    - fixed delete btn on save history - added time progress bars - added theme shicher - fixed timer 3rd time

--- main.js
+++ main.js
@@ -30,13 +30,12 @@ const setCurrentHistory = (index = null) => {
 
 const updateHistory = () => {
 	historyGroup.innerHTML = ''
-	console.log(historyGroup.innerHTML)
 	history.forEach((history, i) => {
 		const content = `
 		<div class="btn-group" role="group">
 			<input type="radio" class="btn-check" name="historyradio" id="historyradio${i}" autocomplete="off" oninput="setCurrentHistory(${i})">
 			<label class="btn btn-outline-secondary" for="historyradio${i}"><i>${history.time}</i>: <b>${history.name}</b></label>
-			<button class="btn btn-outline-danger" onclick="de"><i class="bi bi-trash-fill"></i></button>
+			<button class="btn btn-outline-danger delete-btn" onclick="deleteHistory(${i})"><i class="bi bi-trash-fill"></i></button>
 		</div>`
 		historyGroup.insertAdjacentHTML('beforeEnd', content)
 	})
@@ -72,24 +71,77 @@ deleteBtn.addEventListener('click', () => {
 	}
 	deleteHistory()
 })
+let intervalId = null
+
+const mProgress = document.getElementById('mProgress')
+const hProgress = document.getElementById('hProgress')
+
+const storedTimer = sessionStorage.getItem('timer')
+let timer = storedTimer ? storedTimer : 0
+const timerText = document.getElementById('timerText')
 
 const setTimer = () => {
 	const h = Math.floor(timer / (60 * 60))
 	const m = Math.floor(timer / 60) - h * 60
 	const s = timer % 60
 
-	const mText = h > 0 ? h + 'h ' : ''
-	const hText = m > 0 ? m + 'm ' : ''
+	const hText = h > 0 ? h + 'm ' : ''
+	const mText = m > 0 ? m + 'h ' : ''
 	const sText = s + 's'
+
+	mProgress.style.width = (s / 60) * 100 + '%'
+	mProgress.closest('.progress').setAttribute('aria-valuenow', s)
+	hProgress.style.width = (m / 60) * 100 + '%'
+	hProgress.closest('.progress').setAttribute('aria-valuenow', m)
 	timerText.innerText = `${hText}${mText}${sText}`
 	sessionStorage.setItem('timer', timer)
 	timer++
 }
-const storedTimer = sessionStorage.getItem('timer')
-let timer = storedTimer ? storedTimer : 0
-const timerText = document.getElementById('timerText')
+
 setTimer()
 
-setInterval(() => {
+intervalId = setInterval(() => {
 	setTimer()
 }, 1000)
+
+const checkTabFocused = () => {
+	if (document.visibilityState === 'visible') {
+		intervalId = setInterval(() => {
+			setTimer()
+		}, 1000)
+	} else {
+		clearInterval(intervalId)
+		intervalId = null
+	}
+}
+
+document.addEventListener('visibilitychange', checkTabFocused)
+
+const changeTheme = theme => {
+	localStorage.setItem('theme', theme)
+	document.getElementsByTagName('body')[0].dataset.bsTheme = theme
+
+	document.querySelectorAll('#theme label').forEach(label => {
+		if (theme === 'dark') {
+			label.classList.remove('btn-outline-dark')
+			label.classList.add('btn-outline-light')
+		} else {
+			label.classList.add('btn-outline-dark')
+			label.classList.remove('btn-outline-light')
+		}
+	})
+}
+
+const storedTheme = localStorage.getItem('theme')
+const theme = storedTheme ? storedTheme : 'dark'
+
+document.getElementById('theme' + theme.charAt(0).toUpperCase() + theme.slice(1)).checked = true
+
+changeTheme(theme)
+
+document.querySelectorAll('#theme input').forEach(radio => {
+	radio.addEventListener('change', () => {
+		const theme = radio.value
+		changeTheme(theme)
+	})
+})
